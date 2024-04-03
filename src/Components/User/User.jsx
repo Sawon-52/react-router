0b7c031a366ec92b705a9a6400b31d991673607d
Sawon-52 <mehedihasansawon.52@gmail.com>
2024-04-03 20:10:46 +0600

    using navigate hook for changing route

--- src/Components/User/User.jsx
+++ src/Components/User/User.jsx
@@ -1,16 +1,23 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const User = ({ user }) => {
   const { id, name, email, address } = user;
+  const navigate = useNavigate();
+
+  const handleShow = () => {
+    navigate(`/user/${id}`);
+  };
+
   return (
     <div>
       <div className="border-4 border-red-500 p-4 rounded-md text-xl font-bold ">
         <h1>{name}</h1>
         <h2>{email}</h2>
         <h2>{address.city}</h2>
-        <Link className="text-blue-500 hover:underline flex justify-center p-4 border-2 border-green-400 rounded-lg my-4" to={`/user/${id}`}>
+        <button onClick={handleShow} className="text-blue-500 hover:underline flex justify-center p-4 border-2 border-green-400  rounded-lg my-4">
           Show Details
-        </Link>
+        </button>
+        {/* <Link to={`/user/${id}`}>Show Details</Link> */}
       </div>
     </div>
   );
--- src/Components/UserDetails/UserDetails.jsx
+++ src/Components/UserDetails/UserDetails.jsx
@@ -1,8 +1,12 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useNavigate } from "react-router-dom";
 
 const UserDetails = () => {
   const user = useLoaderData();
   const { name, email, address, website, company } = user;
+  const navigate = useNavigate();
+  const handleBack = () => {
+    navigate(-1);
+  };
   return (
     <div className="md:w-2/3 mx-auto p-4 border-4 border-blue-950 rounded-lg text-3xl font-bold   ">
       <h2 className="text-red-700">Name: {name}</h2>
@@ -10,6 +14,9 @@ const UserDetails = () => {
       <h2>Address: {address.city}</h2>
       <h2>Website: {website}</h2>
       <h2>Company: {company.name}</h2>
+      <button onClick={handleBack} className="text-blue-500 hover:underline flex justify-center p-4 border-2 border-green-400  rounded-lg my-4">
+        Back
+      </button>
     </div>
   );
 };
