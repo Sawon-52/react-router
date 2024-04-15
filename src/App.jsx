535bf1a8c9f21d7d0d3ae6b789d089d2eb5225b3
Sawon-52 <mehedihasansawon.52@gmail.com>
2024-04-15 13:42:37 +0600

    adding loding spinner

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import "./App.css";
 import Header from "./Components/Header/Header";
 import Footer from "./Components/Footer/Footer";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 
 function App() {
+  const navigation = useNavigation();
+
   return (
     <div className="md:w-2/3 mx-auto">
       <Header></Header>
+      {navigation.state === "loading" ? "Loading....." : ""}
+
       <div className="w-full min-h-[400px] bg-gray-200 rounded-md p-5">
         <Outlet></Outlet>
       </div>
+
       <Footer></Footer>
     </div>
   );
