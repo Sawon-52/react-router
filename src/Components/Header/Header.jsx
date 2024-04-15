7c51964a3ac446eabda1cf73b29138bc9055ea75
Sawon-52 <mehedihasansawon.52@gmail.com>
2024-04-15 11:36:31 +0600

    active link using Navlink

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
@@ -9,25 +9,26 @@ const Header = () => {
         </div>
         <div>
           <ul className="flex gap-8">
-            <li>
-              <Link className="text-xl font-semibold hover:underline" to={`/home`}>
+            <li className="text-xl font-semibold hover:underline">
+              <NavLink to={`/home`} className={({ isActive }) => (isActive ? "text-red-500" : "text-gray-900")}>
                 Home
-              </Link>
+              </NavLink>
             </li>
-            <li>
-              <Link className="text-xl font-semibold hover:underline" to={`/about`}>
+            <li className="text-xl font-semibold hover:underline">
+              <NavLink to={`/about`} className={({ isActive }) => (isActive ? "text-red-500" : "text-gray-900")}>
                 About
-              </Link>
+              </NavLink>
             </li>
-            <li>
-              <Link className="text-xl font-semibold hover:underline" to={`/contact`}>
+            <li className="text-xl font-semibold hover:underline">
+              <NavLink to={`/contact`} className={({ isActive }) => (isActive ? "text-red-500" : "text-gray-900")}>
                 Contact
-              </Link>
+              </NavLink>
             </li>
-            <li>
-              <Link className="text-xl font-semibold hover:underline" to={`/users`}>
+
+            <li className="text-xl font-semibold hover:underline">
+              <NavLink to={`/users`} className={({ isActive }) => (isActive ? "text-red-500" : "text-gray-900")}>
                 Users
-              </Link>
+              </NavLink>
             </li>
           </ul>
         </div>
